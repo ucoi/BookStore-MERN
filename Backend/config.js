5fdf634648d70ff5a26152c61dac246a0b354e0a
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-02-20 14:37:51 +0100

    added the db schema

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,3 @@
 export const port = 3000;
+export const db =
+  "mongodb+srv://root:root@bookstore.knt6q.mongodb.net/?retryWrites=true&w=majority&appName=BookStore";
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,24 @@
 import express from "express";
-import { port } from "./config.js";
+import { port, db } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
 const app = express();
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB");
+    console.log(error);
+  });
